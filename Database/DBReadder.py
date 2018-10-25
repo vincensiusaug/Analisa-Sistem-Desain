df700785a366053ebb3dcc3111d603397e3daef1
Vincensius Augustino <vincensius.august@gmail.com>
2018-10-25 20:42:11 +0700

    Auto Push

--- Database/DBReadder.py
+++ Database/DBReadder.py
@@ -3,6 +3,8 @@ import sqlite3
 conn = sqlite3.connect('E-Commerce.db')
 c = conn.cursor()
 
-print("%3s | %10s | %10s | %10s"%("ID", "Name", "Username", "Password"))
+pformat = "| {:^4} | {:^10} | {:^10} | {:^10} |"
+
+print(pformat.format("ID", "Name", "Username", "Password"))
 for row in c.execute('SELECT * FROM User ORDER BY userId'):
-    print("%3s | %10s | %10s | %10s"%(row[0], row[1], row[2], row[3]))
+    print(pformat.format(row[0], row[1], row[2], row[3]))
