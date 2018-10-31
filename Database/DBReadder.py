c24e23439a6a6f4bc178d283b563a507b83e4704
Vincensius Augustino <vincensius.august@gmail.com>
2018-10-31 08:58:58 +0700

    Commit

--- Database/DBReadder.py
+++ Database/DBReadder.py
@@ -4,7 +4,11 @@ connection = sqlite3.connect('E-Commerce.db')
 cursor = connection.cursor()
 
 pformat = "| {:^4} | {:^4} | {:^10} | {:^10} | {:^10} |"
+pformat1 = "| {:-^4} | {:-^4} | {:-^10} | {:-^10} | {:-^10} |"
 
+print(pformat1.format("-","-","-","-","-"))
 print(pformat.format("ID", "Type", "Name", "Username", "Password"))
-for row in cursor.execute('SELECT * FROM User ORDER BY userId'):
+print(pformat1.format("-","-","-","-","-"))
+for row in cursor.execute('SELECT * FROM User ORDER BY id'):
     print(pformat.format(row[0], row[1], row[2], row[3], row[4]))
+print(pformat1.format("-","-","-","-","-"))
