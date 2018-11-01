fe1f05153d5155e720c79c29a1c801d909864e4e
Vincensius Augustino <vincensius.august@gmail.com>
2018-11-02 01:39:30 +0700

    Auto Push

--- PythonFlask/Flask_E-Commerce.py
+++ PythonFlask/Flask_E-Commerce.py
@@ -1,6 +1,8 @@
 from flask import Flask, url_for, render_template
 from DBReader import ReadUsernameInfo, ReadAllUser
+from Forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '12h9817ywd198whd918hw10921027109wjd109284vu12fn10'
 title = 'VT Shop'
 
 @app.route('/')
@@ -12,13 +14,19 @@ def Home():
 def About():
     return render_template('about.html', title=title+' - About')
 
+# @app.route('/login')
+# def Login():
+#     return render_template('Login.html', title=title+' - Login')
+
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = RegistrationForm()
+    return render_template('register.html', title=title+' - Register', form=form)
+
 @app.route('/login')
 def Login():
-    return render_template('login.html', title=title+' - Login')
-
-@app.route('/register')
-def Register():
-    return render_template('register.html', title=title+' - Register')
+    form = LoginForm()
+    return render_template('login.html', title=title+' - Login', form=form)
 
 @app.route('/allUser')
 @app.route('/allUser/')
@@ -30,7 +38,7 @@ def AllUser():
 @app.route('/user/<username>')
 def User(username=None):
     ReadUsernameInfo(username)
-    return render_template('userInfo.html',title=title+' - ', info=ReadUsernameInfo(username))
+    return render_template('userInfo.html',title=title+' - '+username, info=ReadUsernameInfo(username))
 
 @app.errorhandler(404)
 def page_not_found(e):
