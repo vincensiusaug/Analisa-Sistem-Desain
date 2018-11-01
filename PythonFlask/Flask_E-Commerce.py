25d9a677f65ad775f69b4fb61ca18213e94eaef4
Vincensius Augustino <vincensius.august@gmail.com>
2018-11-01 17:08:08 +0700

    Auto Push

--- PythonFlask/Flask_E-Commerce.py
+++ PythonFlask/Flask_E-Commerce.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 title = 'Surabaya Tech Shop'
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', title=title)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title=title)
