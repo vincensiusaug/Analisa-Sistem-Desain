db0016e01418a9c8f78a967619235d48c2600d1c
Vincensius Augustino <vincensius.august@gmail.com>
2018-11-01 17:44:49 +0700

    Auto Push

--- PythonFlask/Flask_E-Commerce.py
+++ PythonFlask/Flask_E-Commerce.py
@@ -1,13 +1,20 @@
 from flask import Flask, url_for, render_template
 from DBReader import ReadUserNameInfo
 app = Flask(__name__)
-title = 'Surabaya Tech Shop'
+title = 'VT Shop'
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title=title)
+@app.route('/home')
+def Home():
+    return render_template('home.html', title=title+' - Home')
 
 @app.route('/about')
-def about():
-    return render_template('about.html', title=title)
+def About():
+    return render_template('about.html', title=title+' - About')
+
+@app.route('/login')
+def Login():
+    return render_template('login.html', title=title+' - Login')
+
+if __name__ == '__main__':
+    app.run(debug=True)
