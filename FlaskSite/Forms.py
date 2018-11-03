866f0b3e49db78b78b30bb584bd283b9428662be
Vincensius Augustino <vincensius.august@gmail.com>
2018-11-03 15:47:08 +0700

    Auto Push

--- FlaskSite/Forms.py
+++ FlaskSite/Forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from FlaskSite.Models import  User
@@ -32,5 +33,18 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-class UserForm(FlaskForm):
-    home = SubmitField('Home')
+class EditProfileForm(FlaskForm):
+    firstName = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    lastName = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=20)])
+    phone = StringField('Phone Number', validators=[DataRequired(), Length(min=8, max=20)])
+    bank = StringField('Bank Number', validators=[DataRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Edit')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('This email is taken.')
+
