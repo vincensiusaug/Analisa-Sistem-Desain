487719e5ca09616d0c647837b378eb1500c51412
Vincensius Augustino <vincensius.august@gmail.com>
2018-12-07 14:32:06 +0700

    Auto Push

--- FlaskSite/Admin.py
+++ FlaskSite/Admin.py
@@ -13,19 +13,17 @@ class ViewPermission(ModelView):
                 return True 
         return False
 
-class FlaskyAdminIndexView(AdminIndexView):
+class OwnerIndexView(AdminIndexView):
     @expose('/')
     def index(self):
         if current_user.is_authenticated:
             if current_user.usertype.name == "Owner":
-                return super(FlaskyAdminIndexView, self).index()
+                return super(OwnerIndexView, self).index()
         return redirect(url_for('main.Home'))
 
 
 
-admin = Admin(app, name='VT Shop', index_view=FlaskyAdminIndexView())
-# admin.add_link(MenuLink(name='Public Website'))
-# admin.add_link('google.com')
+admin = Admin(app, name='VT Shop', index_view=OwnerIndexView(url='/vtshop_owner'))
 admin.add_link(MenuLink(name='Back', url='/'))
 admin.add_view(ViewPermission(User, db.session))
 admin.add_view(ViewPermission(Item, db.session))
