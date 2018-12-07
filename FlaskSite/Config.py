57483e99023c0fc01185c69ed940f92d3602e8e1
Vincensius Augustino <vincensius.august@gmail.com>
2018-12-07 18:59:52 +0700

    Auto Push

--- FlaskSite/Config.py
+++ FlaskSite/Config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///static/Database/Site.db'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///static/Database/Site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
     SEND_FILE_MAX_AGE_DEFAULT = 0
